5bb20c179182197bfda2f35d407e762140e9f812
manishyoudumb <darksalterego99@gmail.com>
2024-07-15 09:40:45 +0530

    Update CLI commands

--- src/auth.rs
+++ src/auth.rs
@@ -10,12 +10,38 @@ pub async fn login() -> Result<(), Box<dyn Error>> {
 
     let (auth_url, _) = client 
         .authorize_url(oauth2::CsrffToken::new_random)
-       .add_scope(Scope::new("https://www.googleapis.com/auth/tasks".to_string()))
+        .add_scope(Scope::new("https://www.googleapis.com/auth/tasks".to_string()))
         .url();
 
     println!("Please Open this URL in your browser to login:\n\n{}\n\n",auth_url);
-    
 
+    let Listener = TcpListener::bind("127.0.0.1:8080")?;
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            let code; 
+            {
+                let mut reader = BufReader::new(&stream);
+                let mut request_line = String::new();
+                reader.read_line(&mut request_line)?;
+
+                let redirect_url = request_line.split_whitespace().nth(1).unwrap();
+                let url = url::Url::parse(&("http://localhost".to_string() + redirect_url))?;
+
+                let code_pair = url
+                    .query_pairs()
+                    .find(|pair| {
+                        let &(ref key, _) = pair;
+                        key == "code"
+                    })
+                    .unwrap();
+
+                let (_, value) = code_pair;
+                code = value.into_owned();
+            }
+
+            
+        }
+    }
     
 
 }
