f7c6600d38b5e7e3c01af1eca2c281b044f55968
manishyoudumb <darksalterego99@gmail.com>
2024-07-17 00:48:12 +0530

    Added test module with async test for push and pull changes, and updated models to derive Clone trait.

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,13 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 
 pub(crate) struct List {
     pub name: String,
     pub items: Vec<Item>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub(crate) struct Item {
     pub description: String,
     pub completed: bool,
