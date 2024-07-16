44654bf31fbdbdee896b177edc5aa0d774d5f9c8
manishyoudumb <darksalterego99@gmail.com>
2024-07-16 09:43:02 +0530

    Added `push_changes` method to `Database` struct and imported `anyhow::Ok`

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use futures::TryStreamExt;
 use mongodb::bson;
 use mongodb::{Client, Database as MongoDatabase, bson::doc, options::ClientOptions};
@@ -7,4 +8,28 @@ use crate::error::{TodoError, TodoResult};
 
 pub struct Database {
     db: MongoDatabase,
+}
+
+impl Database {
+    
+    pub async fn push_changes(&self) -> TodoResult<()> {
+        let collection = self.db.collection::<List>("lists");
+        let local_lists = self.get_lists().await?;
+        
+        for list in local_lists {
+            collection.replace_one(
+                doc! { "name": &list.name },
+                &list,
+                mongodb::options::ReplaceOptions::builder().upsert(true).build(),
+            ).await?;
+        }
+        
+        Ok(())
+
+    }
+
+    
+
+
+
 }
