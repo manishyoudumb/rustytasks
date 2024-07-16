cce3f06dd7842d3d0aac92334aeb8a253d080646
manishyoudumb <darksalterego99@gmail.com>
2024-07-16 09:32:50 +0530

    Added new error variants to TodoError enum and created TodoResult type alias

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,18 @@ pub enum TodoError {
     #[error("Database error: {0}")]
     DatabaseError(#[from] mongodb::error::Error),
 
-    
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Invalid input: {0}")]
+    InvalidInput(String),
+
+    #[error("Item not found: {0}")]
+    ItemNotFound(String),
+
+    #[error("List not found: {0}")]
+    ListNotFound(String),
 
 }
     
+pub type TodoResult<T> = Result<T, TodoError>;
